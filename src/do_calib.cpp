2faef7a1c8f9c24de192c279bcccd0202e0fba8b
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2020-12-03 14:01:38 +0200

    change from 500 samples to 250

--- src/do_calib.cpp
+++ src/do_calib.cpp
@@ -51,7 +51,7 @@ DoCalib::DoCalib() :
   imu_sub_ = nh.subscribe("imu", 1, &DoCalib::imuCallback, this);
 
   ros::NodeHandle nh_private("~");
-  nh_private.param<int>("measurements", measurements_per_orientation_, 500);
+  nh_private.param<int>("measurements", measurements_per_orientation_, 250);
   nh_private.param<double>("reference_acceleration", reference_acceleration_, 9.80665);
   nh_private.param<std::string>("output_file", output_file_, "imu_calib.yaml");
 
